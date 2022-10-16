21be2403f219f53d0bbc80554de834df6b82135e
Ruslan Sabirov <>
2022-10-16 18:37:27 +0300

    Add imageView extension

---
+++ AM-Exercise/Helpers/UIImageView+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Ext.swift
+//  AM-Exercise
+//
+//  Created by Руслан Сабиров on 16/10/2022.
+//  Copyright © 2022 Michael Mavris. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage{
+    var roundedImage: UIImage? {
+        let rect = CGRect(origin:CGPoint(x: 0, y: 0), size: self.size)
+        UIGraphicsBeginImageContextWithOptions(self.size, false, 1)
+        UIBezierPath(
+            roundedRect: rect,
+            cornerRadius: 10
+            ).addClip()
+        self.draw(in: rect)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
